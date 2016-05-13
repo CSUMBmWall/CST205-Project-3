6cf5eb416217cd7fd4824d899149dcec7ad6876d
Matt <mwall@csumb.edu>
2016-05-13 16:23:54 -0700

    fresh files.  Works with Bokeh

---
+++ src/libraries.py
@@ -0,0 +1,17 @@
+# sound_analysis.py
+from scipy.io.wavfile import read
+from scipy.signal import hann
+from scipy.fftpack import rfft, fft
+import matplotlib as mpl
+mpl.use("TkAgg")
+import matplotlib.pyplot as plt
+import scipy
+import numpy as np
+import wave
+import sys
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.charts import Scatter, output_file, show
+from bokeh.io import vform
+from bokeh.models import CustomJS, ColumnDataSource, Slider
+
